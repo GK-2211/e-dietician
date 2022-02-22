9969f16ada6e1c03d569314a3df7a0e20ef9c159
GK-2211 <84940658+GK-2211@users.noreply.github.com>
2022-02-23 03:38:56 +0530

    second

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-landing-page',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private viewportscroller : ViewportScroller) { }
 
   ngOnInit(): void {
   }
 
+  onclick(id : string){
+    this.viewportscroller.scrollToAnchor(id);
+  }
 }
